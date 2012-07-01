02706c3b6e34b4976e11974aa01d8c6e0c017d5f
Patrick Glandien <patrick@pagla.de>
2012-07-01 20:03:17 +0200

    Make it work with a Zurb Foundation wrapper (and generally more agnostic)

--- lib/client_side_validations/simple_form/form_builder.rb
+++ lib/client_side_validations/simple_form/form_builder.rb
@@ -5,19 +5,18 @@ module ClientSideValidations
       def self.included(base)
         base.class_eval do
           def self.client_side_form_settings(options, form_helper)
-            #wrapper_object = ::SimpleForm.wrappers[options[:wrapper]] || ::SimpleForm.wrappers[:default]
-            wrapper_object = ::SimpleForm.wrappers[:bootstrap]
+            wrapper_object = ::SimpleForm.wrappers[options[:wrapper]] || ::SimpleForm.wrappers[::SimpleForm.default_wrapper]
             input_error = wrapper_object.find(:error).defaults
             wrapper = wrapper_object.defaults
 
-            error_container_class = input_error[:class] # help-block
-            input_error_tag = input_error[:tag] # "span"
-            error_tag = wrapper[:class].join(" ") #clearfix
-            error_class = wrapper[:error_class] # error
+            error_container_class = input_error[:class]
+            input_error_tag = input_error[:tag]
+            error_tag = wrapper[:class].join(" ")
+            error_class = wrapper[:error_class]
 
             {
               :type => self.to_s,
-              :error_class => [error_container_class, error_class].join(" ."),
+              :error_class => [error_container_class, error_class].reject { |e| e.blank? }.join(" ."),
               :error_tag => input_error_tag,
               :wrapper_error_class => error_class,
               :wrapper_tag => ".#{error_tag}"
